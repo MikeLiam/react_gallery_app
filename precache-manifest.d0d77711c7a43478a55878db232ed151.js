36469b92a222696ce6bee236a03e630e56b76b80
MikeLiam <miguelalcalaromero@gmail.com>
2020-09-29 16:06:41 +0200

    Updates

---
+++ precache-manifest.d0d77711c7a43478a55878db232ed151.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3671e291f348f7f7607eb449f8b744b7",
+    "url": "/react_gallery_app/index.html"
+  },
+  {
+    "revision": "3548a55c87f16a6e5682",
+    "url": "/react_gallery_app/static/css/main.bc0cedb0.chunk.css"
+  },
+  {
+    "revision": "98f0380a3b9033a7ebb7",
+    "url": "/react_gallery_app/static/js/2.f2662377.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_gallery_app/static/js/2.f2662377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3548a55c87f16a6e5682",
+    "url": "/react_gallery_app/static/js/main.2ce7f4f0.chunk.js"
+  },
+  {
+    "revision": "96f3138e8107556eacb3",
+    "url": "/react_gallery_app/static/js/runtime-main.f61bdbbc.js"
+  },
+  {
+    "revision": "d8b74ad1a0f739304484132caeb1b230",
+    "url": "/react_gallery_app/static/media/moto.d8b74ad1.png"
+  }
+]);
